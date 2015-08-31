04588ebf8d174ed267b3ee0621ba6bbc7afd2a36
Daniel Salunga <dsalunga@live.com>
2015-08-31 17:45:01 +0800

    Adding mongoose, consolidate

---
+++ utils/mongoose.js
@@ -0,0 +1,24 @@
+/**
+ * Created by dcsalunga on 21/8/15.
+ */
+
+var mongoose = require('mongoose'),
+    kraken = require('../app').kraken;
+
+module.exports = function(){
+    var uri = kraken.get('mongodb:uri');
+
+    console.log('[mongoose] connecting');
+    mongoose.connect(uri, {server:{auto_reconnect:true}});
+    var db = mongoose.connection;
+
+    db.on('error', function(error) {
+        mongoose.disconnect();
+        console.log('[mongoose] disconnect');
+    });
+
+    db.on('disconnected', function() {
+        mongoose.connect(uri, {server:{auto_reconnect:true}});
+        console.log('[mongoose] reconnect');
+    });
+}
