2985c1dff6a049e371ff230e043d5a65b6524dec
Daniel Salunga <dsalunga@live.com>
2015-10-20 18:14:11 +0800

    Adding Content schema, Sort in topic and argument, render raw html for content

---
+++ models/core/Content.js
@@ -0,0 +1,18 @@
+'use strict';
+
+exports = module.exports = function(app, mongoose) {
+  var schema = new mongoose.Schema({
+    id: { type: String },
+    content: { type: String, default: '' },
+    title: { type: String, default: '' },
+    createDate: { type: Date, default: Date.now },
+    createUserId: { type: mongoose.Schema.ObjectId, ref: 'User' },
+    editDate: { type: Date, default: Date.now },
+    editUserId: { type: mongoose.Schema.ObjectId, ref: 'User' }
+  });
+  schema.plugin(require('../plugins/pagedFind'));
+  schema.index({ id: 1 });
+  schema.index({ title: 1 });
+  schema.set('autoIndex', (app.get('env') === 'development'));
+  app.db.model('Content', schema);
+};
