8af36b49606d355dd1c5654f531555191530c938
Daniel Salunga <dsalunga@live.com>
2015-10-21 18:36:08 +0800

    Restructure menus

---
+++ controllers/morality/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = function (router) {
+    var model = {};
+
+    router.get('/', function (req, res) {
+        res.render('dust/morality/index', model);
+    });
+};
