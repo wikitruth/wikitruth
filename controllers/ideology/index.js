1a14f084e0bec3dfba4aad941cdbab23082fbbba
Daniel Salunga <dsalunga@live.com>
2015-11-05 01:56:23 +0800

    Religion hierarchy support

--- controllers/ideology/index.js
+++ controllers/ideology/index.js
@@ -1,50 +1,121 @@
 'use strict';
 
-var utils = require('../../utils/utils'),
-    wikiUtils = require('../../utils/wikiUtils'),
-    mongoose = require('mongoose'),
-    constants = require('../../models/constants'),
-    db = require('../../app').db.models;
+var utils       = require('../../utils/utils'),
+    wikiUtils   = require('../../utils/wikiUtils'),
+    mongoose    = require('mongoose'),
+    constants   = require('../../models/constants'),
+    async       = require('async'),
+    db          = require('../../app').db.models;
+
+function setItemModel(req, model, callback) {
+    if(req.query.id) {
+        db.Ideology.findOne({_id: req.query.id}, function (err, result) {
+            model.item = result;
+            wikiUtils.appendOwnerFlag(req, result, model);
+            callback();
+        });
+    } else {
+        callback();
+    }
+}
+
+function setItemModels(req, model, callback) {
+    if(req.query.id) {
+        async.series({
+            item: function (callback) {
+                db.Ideology.findOne({_id: req.query.id}, function(err, result) {
+                    if(result) {
+                        model.item = result;
+                        wikiUtils.appendOwnerFlag(req, result, model);
+                    }
+                    callback();
+                });
+            },
+            parent: function (callback) {
+                if(model.item && model.item.parentId) {
+                    db.Ideology.findOne({_id: model.item.parentId}, function (err, result) {
+                        if (result) {
+                            model.parent = result;
+                        }
+                        callback();
+                    });
+                } else {
+                    callback();
+                }
+            }
+        }, function (err, results) {
+            callback();
+        });
+    } else {
+        callback();
+    }
+}
 
 module.exports = function (router) {
 
     router.get('/', function (req, res) {
         var model = {};
-        db.Ideology.find({}).limit(100).sort({ title: 1 }).exec(function(err, results) {
-            results.forEach(function(result) {
-                result.comments = utils.numberWithCommas(utils.randomInt(1,100000));
-            });
-            model.items = results;
+        async.parallel({
+            item: function(callback){
+                setItemModels(req, model, callback);
+            },
+            items: function(callback) {
+                var query = req.query.id ? { parentId: req.query.id } : { parentId: null};
+                db.Ideology.find(query).limit(100).sort({ title: 1 }).exec(function(err, results) {
+                    results.forEach(function(result) {
+                        result.comments = utils.numberWithCommas(utils.randomInt(1,100000));
+                    });
+                    model.items = results;
+                    callback();
+                });
+            }
+        }, function (err, results) {
             res.render('dust/ideology/index', model);
         });
     });
 
     router.get('/entry', function (req, res) {
         var model = {};
-        db.Ideology.findOne({_id: req.query.id}, function(err, result) {
-            model.item = result;
-            wikiUtils.appendOwnerFlag(req, result, model);
+        setItemModels(req, model, function() {
             res.render('dust/ideology/item', model);
         });
     });
 
     router.get('/create', function (req, res) {
         var model = {};
-        if(req.query.id) {
-            db.Ideology.findOne({_id: req.query.id}, function (err, result) {
-                model.item = result;
-                res.render('dust/ideology/create', model);
-            });
-        } else {
+        async.series({
+            item: function(callback){
+                if(req.query._id) {
+                    db.Ideology.findOne({_id: req.query._id}, function (err, result) {
+                        model.item = result;
+                        callback();
+                    });
+                } else {
+                    callback();
+                }
+            },
+            parent: function(callback) {
+                var query = {
+                    _id: req.query.id ? req.query.id : model.item && model.item.parentId ? model.item.parentId : null
+                };
+                if(query._id) {
+                    db.Ideology.findOne(query, function (err, result) {
+                        model.parent = result;
+                        callback();
+                    });
+                } else {
+                    callback();
+                }
+            }
+        }, function (err, results) {
             res.render('dust/ideology/create', model);
-        }
+        });
     });
 
     router.post('/create', function (req, res) {
         var query = {
-            _id: req.query.id ? req.query.id : new mongoose.Types.ObjectId()
+            _id: req.query._id ? req.query._id : new mongoose.Types.ObjectId()
         };
-
         db.Ideology.findOne(query, function(err, result) {
             var entity = result ? result : {};
             entity.content = req.body.content;
@@ -55,14 +126,15 @@ module.exports = function (router) {
                 entity.createUserId = req.user.id;
                 entity.createDate = Date.now();
             }
-
+            entity.parentId = req.body.parent ? req.body.parent : null;
             db.Ideology.update(query, entity, {upsert: true}, function(err, writeResult) {
                 if (err) {
                     throw err;
                 }
-
                 if(result) {
-                    res.redirect('/ideology/entry?id=' + req.query.id);
+                    res.redirect('/ideology/entry?id=' + req.query._id);
+                } else if(req.query.id) {
+                    res.redirect('/ideology?id=' + req.query.id);
                 } else {
                     res.redirect('/ideology');
                 }
@@ -74,23 +146,18 @@ module.exports = function (router) {
 
     router.get('/questions', function (req, res) {
         var model = {};
-        if(req.query.id) {
-            db.Ideology.findOne({_id: req.query.id}, function (err, result) {
-                model.item = result;
-                wikiUtils.appendOwnerFlag(req, result, model);
-                res.render('dust/ideology/questions', model);
-            });
-        } else {
-            // Top Questions
-            // TODO: Filter top 100 based on number of activities
+        setItemModel(req, model, function() {
+            if(!req.query.id) {
+                // Top Questions
+                // TODO: Filter top 100 based on number of activities
+            }
             res.render('dust/ideology/questions', model);
-        }
+        });
     });
 
     router.get('/question/create', function (req, res) {
         var model = {};
-        db.Ideology.findOne({_id: req.query.id}, function (err, result) {
-            model.item = result;
+        setItemModel(req, model, function() {
             res.render('dust/ideology/question/create', model);
         });
     });
@@ -99,9 +166,7 @@ module.exports = function (router) {
 
     router.get('/related', function (req, res) {
         var model = {};
-        db.Ideology.findOne({_id: req.query.id}, function (err, result) {
-            model.item = result;
-            wikiUtils.appendOwnerFlag(req, result, model);
+        setItemModel(req, model, function() {
             res.render('dust/ideology/related', model);
         });
     });
