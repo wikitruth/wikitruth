9dced79a9c18c37c41d71ce9c12070ea21be4e45
Daniel Salunga <dsalunga@live.com>
2016-08-02 18:38:59 +0800

    Improve home page, navbar

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,60 @@
 'use strict';
 
-var templates   = require('../models/templates');
+var utils       = require('../utils/utils'),
+    templates   = require('../models/templates'),
+    constants   = require('../models/constants'),
+    db          = require('../app').db.models,
+    async       = require('async');
 
 module.exports = function (router) {
-    var model = {};
 
     router.get('/', function (req, res) {
-        res.render(templates.index, model);
+        var model = {};
+        async.parallel({
+            truth: function(callback) {
+                db.Topic.find({parentId: null, groupId: constants.CORE_GROUPS.truth }).limit(3).exec(function (err, results) {
+                    async.each(results, function(result, callback) {
+                        result.comments = utils.numberWithCommas(utils.randomInt(1, 100000));
+                        db.Topic.find( { parentId: result._id } ).limit(2).sort({ title: 1 }).exec(function(err, subtopics) {
+                            result.subtopics = subtopics;
+                            callback();
+                        });
+                    }, function(err) {
+                        model.truth = results;
+                        callback();
+                    });
+                });
+            },
+            worldviews: function(callback) {
+                db.Ideology.find({ parentId: null }).limit(3).sort({ title: 1 }).exec(function(err, results) {
+                    async.each(results, function(result, callback) {
+                        result.comments = utils.numberWithCommas(utils.randomInt(1, 100000));
+                        db.Ideology.find( { parentId: result._id } ).limit(2).exec(function(err, subworldviews) {
+                            result.subworldviews = subworldviews;
+                            callback();
+                        });
+                    }, function(err) {
+                        model.worldviews = results;
+                        callback();
+                    });
+                });
+            },
+            morality: function(callback) {
+                db.Topic.find({parentId: null, groupId: constants.CORE_GROUPS.morality}).limit(3).exec(function (err, results) {
+                    async.each(results, function(result, callback) {
+                        result.comments = utils.numberWithCommas(utils.randomInt(1, 100000));
+                        db.Topic.find( { parentId: result._id } ).limit(2).sort({ title: 1 }).exec(function(err, subtopics) {
+                            result.subtopics = subtopics;
+                            callback();
+                        });
+                    }, function(err) {
+                        model.morality = results;
+                        callback();
+                    });
+                });
+            }
+        }, function (err, results) {
+            res.render(templates.index, model);
+        });
     });
 };
