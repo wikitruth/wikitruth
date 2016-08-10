9cde1d1c97819488b7f677024860489270f45100
Daniel Salunga <dsalunga@live.com>
2016-08-10 18:44:48 +0800

    Mongo restore fix

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -3,7 +3,11 @@
 var templates   = require('../../models/templates'),
     config      = require('../../config/config'),
     backup      = require('mongodb-backup'),
-    restore      = require('mongodb-restore');
+    restore     = require('mongodb-restore'),
+    db          = require('../../app').db.models,
+    mongoose    = require('mongoose'),
+    fs          = require('fs'),
+    async       = require('async');
 
 function getBackupDir() {
     if(config.mongodb.backupRoot) {
@@ -31,16 +35,76 @@ module.exports = function (router) {
             backup({
                 uri: config.mongodb.uri, // mongodb://<dbuser>:<dbpassword>@<dbdomain>.mongolab.com:<dbport>/<dbdatabase>
                 root: getBackupDir(), // write files into this dir
-                collectionsX: [ 'topics', 'arguments' ], // save this collection only
+                //collectionsX: [ 'topics', 'arguments' ], // save this collection only
                 parser: 'json'
             });
         } else if(action === 'restore') {
             restore({
                 uri: config.mongodb.uri, // mongodb://<dbuser>:<dbpassword>@<dbdomain>.mongolab.com:<dbport>/<dbdatabase>
                 root: getBackupDir() + '/wikitruth', // write files into this dir
-                dropCollections: [ 'topics', 'arguments' ], // save this collection only
-                parser: 'json'
+                //dropCollections: [ 'topics', 'arguments' ], // save this collection only
+                parser: 'json',
+                drop: true
             });
+        } else if(action === 'restore2') {
+            var colmaps = {
+                'accountcategories': 'AccountCategory',
+                'accounts': 'Account',
+                'admingroups': 'AdminGroup',
+                'admins': 'Admin',
+                'arguments': 'Argument',
+                'categories': 'Category',
+                'ideologies': 'Ideology',
+                'issues': 'Issue',
+                'loginattempts': 'LoginAttempt',
+                'opinions': 'Opinion',
+                'pages': 'Page',
+                'questions': 'Question',
+                'sessions': 'Session',
+                'status': 'Status',
+                'topics': 'Topic',
+                'users': 'User',
+                'words': 'Word'
+            };
+            var cols = [
+                'users',
+                'accountcategories',
+                'accounts',
+                'admingroups',
+                'admins',
+                'arguments',
+                'ideologies',
+                'issues',
+                'loginattempts',
+                'opinions',
+                'pages',
+                'questions',
+                'status',
+                'topics',
+                'words'
+            ];
+            var dir = getBackupDir() + '/wikitruth';
+            //var cols = fs.readdirSync(dir);
+            console.log(cols);
+            for (var i=0; i<cols.length; i++) {
+                var col = cols[i];
+                var coldir = dir + '/' + col;
+                var jsons = fs.readdirSync(coldir);
+                console.log('restore:', col);
+                console.log(jsons);
+                for (var x=0; x<jsons.length; x++) {
+
+                    var json = jsons[x];
+                    var file = coldir + '/' + json;
+                    var obj = JSON.parse(fs.readFileSync(file, 'utf8'));
+                    if(colmaps[col]) {
+                        db[colmaps[col]].create(obj, function (err, newObj) {
+                            if (err) console.error(err);
+                            // saved!
+                        })
+                    }
+                }
+            }
         }
         model.action = action;
         res.render(templates.admin.mongoBackup, model);
