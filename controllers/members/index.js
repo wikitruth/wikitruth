b1e3cf6e787ccf9e77a3b1eeb250607955b9a004
Daniel Salunga <dsalunga@live.com>
2016-12-16 00:26:05 +0800

    Member contributions, overview, fixes

--- controllers/members/index.js
+++ controllers/members/index.js
@@ -3,8 +3,24 @@
 var templates   = require('../../models/templates'),
     async       = require('async'),
     flowUtils   = require('../../utils/flowUtils'),
+    utils       = require('../../utils/utils'),
     constants   = require('../../models/constants'),
     db          = require('../../app').db.models;
+
+function setMemberModel(model, req, callback) {
+    if(req.params.username) {
+        if (req.user && req.user.username === req.params.username) {
+            model.member = req.user;
+            model.loggedIn = true;
+        } else {
+            return db.User.findOne({username: req.params.username}, function (err, result) {
+                model.member = result;
+                callback();
+            });
+        }
+    }
+    callback();
+}
 
 module.exports = function (router) {
 
@@ -34,56 +50,210 @@ module.exports = function (router) {
 
     router.get('/:username', function (req, res) {
         var model = {};
-        if(req.params.username) {
-            if(req.user && req.user.username === req.params.username) {
-                model.user = req.user;
-                model.loggedIn = true;
-            } else {
-                return db.User.findOne({'username': req.params.username}, function (err, result) {
-                    model.user = result;
-                    res.render(templates.members.profile.index, model);
-                });
-            }
-        }
-        res.render(templates.members.profile.index, model);
+        setMemberModel(model, req, function() {
+            model.url = '/members/' + model.member.username + '/contributions';
+            async.parallel({
+                topics: function(callback) {
+                    db.Topic
+                        .find({ createUserId: model.member._id })
+                        .count(function(err, count) {
+                            model.topics = count;
+                            callback();
+                        });
+                },
+                arguments: function(callback) {
+                    db.Argument
+                        .find({ createUserId: model.member._id })
+                        .count(function(err, count) {
+                            model.arguments = count;
+                            callback();
+                        });
+                },
+                questions: function (callback) {
+                    db.Question
+                        .find({ createUserId: model.member._id })
+                        .count(function(err, count) {
+                            model.questions = count;
+                            callback();
+                        });
+                },
+                issues: function (callback) {
+                    db.Issue
+                        .find({ createUserId: model.member._id })
+                        .count(function(err, count) {
+                            model.issues = count;
+                            callback();
+                        });
+                },
+                opinions: function (callback) {
+                    db.Opinion
+                        .find({ createUserId: model.member._id })
+                        .count(function(err, count) {
+                            model.opinions = count;
+                            callback();
+                        });
+                }
+            }, function (err, results) {
+                model.contributions = model.topics + model.arguments + model.questions + model.issues + model.opinions;
+                res.render(templates.members.profile.index, model);
+            });
+        });
     });
 
     router.get('/:username/contributions', function (req, res) {
         var model = {};
-        if(req.params.username) {
-            if(req.user && req.user.username === req.params.username) {
-                model.user = req.user;
-                model.loggedIn = true;
-            } else {
-                return db.User.findOne({'username': req.params.username}, function (err, result) {
-                    model.user = result;
-                    res.render(templates.members.profile.contributions, model);
-                });
-            }
-        }
-        res.render(templates.members.profile.contributions, model);
+        setMemberModel(model, req, function () {
+            var tab = req.query.tab ? req.query.tab : 'all';
+            model.tab = tab;
+            model.results = tab !== 'all';
+            model.url = '/members/' + model.member.username + '/contributions';
+            async.parallel({
+                topics: function(callback) {
+                    if(tab !== 'all' && tab !== 'topics') {
+                        return callback();
+                    }
+                    db.Topic
+                        .find({ createUserId: model.member._id })
+                        .sort({ title: 1 })
+                        .limit(tab === 'all' ? 15 : 0)
+                        .lean()
+                        .exec(function(err, results) {
+                            if(err || !results) {
+                                callback(err);
+                            }
+                            flowUtils.setEditorsUsername(results, function() {
+                                results.forEach(function (result) {
+                                    result.friendlyUrl = utils.urlify(result.title);
+                                    flowUtils.appendEntryExtra(result);
+                                });
+                                model.topics = results;
+                                if(results.length > 0) {
+                                    if(results.length === 15) {
+                                        model.topicsMore = true;
+                                    }
+                                    model.results = true;
+                                }
+                                callback();
+                            });
+                        });
+                },
+                arguments: function(callback) {
+                    if(tab !== 'all' && tab !== 'arguments') {
+                        return callback();
+                    }
+                    db.Argument
+                        .find({ createUserId: model.member._id })
+                        .sort({ title: 1 })
+                        .limit(tab === 'all' ? 15 : 0)
+                        .lean()
+                        .exec(function(err, results) {
+                            flowUtils.setEditorsUsername(results, function() {
+                                results.forEach(function (result) {
+                                    result.friendlyUrl = utils.urlify(result.title);
+                                    flowUtils.appendEntryExtra(result);
+                                    flowUtils.setVerdictModel(result);
+                                });
+                                model.arguments = results;
+                                if (results.length > 0) {
+                                    if(results.length === 15) {
+                                        model.argumentsMore = true;
+                                    }
+                                    model.results = true;
+                                }
+                                callback();
+                            });
+                        });
+                },
+                questions: function (callback) {
+                    if(tab !== 'all' && tab !== 'questions') {
+                        return callback();
+                    }
+                    db.Question
+                        .find({ createUserId: model.member._id })
+                        .sort({ title: 1 })
+                        .limit(tab === 'all' ? 15 : 0)
+                        .exec(function(err, results) {
+                            flowUtils.setEditorsUsername(results, function() {
+                                results.forEach(function (result) {
+                                    result.friendlyUrl = utils.urlify(result.title);
+                                    result.friendlyUrl = utils.urlify(result.title);
+                                    flowUtils.appendEntryExtra(result);
+                                });
+                                model.questions = results;
+                                if(results.length > 0) {
+                                    model.results = true;
+                                }
+                                callback();
+                            });
+                        });
+                },
+                issues: function (callback) {
+                    if(tab !== 'all' && tab !== 'issues') {
+                        return callback();
+                    }
+                    db.Issue
+                        .find({ createUserId: model.member._id })
+                        .sort({ title: 1 })
+                        .limit(tab === 'all' ? 15 : 0)
+                        .lean()
+                        .exec(function(err, results) {
+                            flowUtils.setEditorsUsername(results, function() {
+                                results.forEach(function (result) {
+                                    result.friendlyUrl = utils.urlify(result.title);
+                                    flowUtils.appendEntryExtra(result);
+                                });
+                                model.issues = results;
+                                if (results.length > 0) {
+                                    if(results.length === 15) {
+                                        model.issuesMore = true;
+                                    }
+                                    model.results = true;
+                                }
+                                callback();
+                            });
+                        });
+                },
+                opinions: function (callback) {
+                    if(tab !== 'all' && tab !== 'opinions') {
+                        return callback();
+                    }
+                    db.Opinion
+                        .find({ createUserId: model.member._id })
+                        .sort({ title: 1 })
+                        .limit(tab === 'all' ? 15 : 0)
+                        .lean()
+                        .exec(function(err, results) {
+                            flowUtils.setEditorsUsername(results, function() {
+                                results.forEach(function (result) {
+                                    result.friendlyUrl = utils.urlify(result.title);
+                                    flowUtils.appendEntryExtra(result);
+                                });
+                                model.opinions = results;
+                                if (results.length > 0) {
+                                    if(results.length === 15) {
+                                        model.opinionsMore = true;
+                                    }
+                                    model.results = true;
+                                }
+                                callback();
+                            });
+                        });
+                }
+            }, function (err, results) {
+                res.render(templates.members.profile.contributions, model);
+            });
+        });
     });
 
     router.get('/:username/topics', function (req, res) {
         var model = {};
         async.series({
             user: function(callback){
-                if(req.params.username) {
-                    if (req.user && req.user.username === req.params.username) {
-                        model.user = req.user;
-                        model.loggedIn = true;
-                    } else {
-                        return db.User.findOne({username: req.params.username}, function (err, result) {
-                            model.user = result;
-                            callback();
-                        });
-                    }
-                }
-                callback();
+                setMemberModel(model, req, callback);
             },
             topics: function(callback) {
                 // display 15 if top topics, all if has topic parameter
-                flowUtils.getTopics({ parentId: null, ownerType: constants.OBJECT_TYPES.user, ownerId: model.user._id }, 0, function (err, results) {
+                flowUtils.getTopics({ parentId: null, ownerType: constants.OBJECT_TYPES.user, ownerId: model.member._id }, 0, function (err, results) {
                     model.topics = results;
                     callback();
                 });
