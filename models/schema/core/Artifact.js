8bfd88c5f89603ae63c9969ed4dd97f8f14d4b1e
Daniel Salunga <dsalunga@live.com>
2017-06-04 14:55:53 +0800

    Some UX fixes, etc

--- models/schema/core/Artifact.js
+++ models/schema/core/Artifact.js
@@ -4,14 +4,19 @@ module.exports = function(app, mongoose) {
   var schema = new mongoose.Schema({
     title: { type: String, default: '' },
     content: { type: String, default: '' },
+    source: { type: String, default: '' },
+    url: { type: String, default: '' },
+    friendlyUrl: { type: String },
+    ownerId: { type: mongoose.Schema.ObjectId },
+    ownerType: { type: Number }, // MODEL_TYPES
+    categoryId: { type: mongoose.Schema.ObjectId, ref: 'Topic' }, // the root topic where this entry belong
     createDate: { type: Date, default: Date.now },
     createUserId: { type: mongoose.Schema.ObjectId, ref: 'User' },
     editDate: { type: Date, default: Date.now },
     editUserId: { type: mongoose.Schema.ObjectId, ref: 'User' }
   });
   schema.plugin(require('../plugins/pagedFind'));
-  schema.index({ id: 1 });
-  schema.index({ word: 1 });
+  schema.index({ title: 1 });
   schema.set('autoIndex', (app.get('env') === 'development'));
-  app.db.model('Word', schema);
+  app.db.model('Artifact', schema);
 };
