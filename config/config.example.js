6d87b32e48474886699fd2ced555679fc9f5c587
Daniel Salunga <dsalunga@live.com>
2019-05-28 15:02:17 +0800

    Support Express trust proxy via config

--- config/config.example.js
+++ config/config.example.js
@@ -61,6 +61,7 @@ exports.titleSlogan = 'Wikitruth, the truth in totality of human knowledge';
 exports.systemEmail = 'your@email.addy';
 exports.cryptoKey = 'abc123';
 exports.cacheBreaker = 'abc123';
+exports.trustProxy = false;
 exports.loginAttempts = {
   forIp: 50,
   forIpAndUser: 7,
