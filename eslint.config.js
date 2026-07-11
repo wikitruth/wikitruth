4e95edc9566d97178736b7201c04af985c525da9
Daniel Salunga <dsalunga@live.com>
2026-07-12 02:25:00 +0800

    chore(lint): migrate to ESLint 9 flat config

---
+++ eslint.config.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const eslint = require('@eslint/js');
+const globals = require('globals');
+const prettier = require('eslint-config-prettier');
+const react = require('eslint-plugin-react');
+const reactHooks = require('eslint-plugin-react-hooks');
+const typescriptEslint = require('@typescript-eslint/eslint-plugin');
+
+module.exports = [
+  {
+    ignores: ['node_modules/', 'public/components/', '.build/', 'coverage/', 'react-tutorial/'],
+    linterOptions: {
+      reportUnusedDisableDirectives: false,
+    },
+  },
+  eslint.configs.recommended,
+  ...typescriptEslint.configs['flat/recommended'],
+  react.configs.flat.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-this-alias': ['warn', { allowedNames: ['self'] }],
+      'no-case-declarations': 'warn',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      'react/no-unescaped-entities': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    files: ['**/*.stories.tsx', '**/*.stories.ts'],
+    rules: {
+      'react-hooks/rules-of-hooks': 'off',
+    },
+  },
+  prettier,
+];
